194aa404db5ec2f3711cba47bb16949f2e54491d
dawn-yoo <158089325+dawn-yoo@users.noreply.github.com>
2024-03-27 17:31:30 -0400

    Update rs.sv
    made another row
    op.tag

--- verilog/rs.sv
+++ verilog/rs.sv
@@ -13,76 +13,74 @@ module rs(
 	input reset,
 	input ID_EX_PACKET op,
 	input TAG T, T1, T2, CDB,
-	output rs_busy_alu, rs_busy_mul, rs_busy_ld, rs_busy_st);
+	output rs_busy_alu, rs_busy_fp1, rs_busy_fp2, rs_busy_ld, rs_busy_st
+	);
 
 	logic RS_ENTRY [`RS_SZ -1:0] rs_table;
 	int 
 
 
 assign rs_busy_alu = rs_table[0].busy;
-assign rs_busy_mul = rs_table[1].busy;
-assign rs_busy_ld  = rs_table[2].busy;
-assign rs_busy_st  = rs_table[3].busy;
+assign rs_busy_ld  = rs_table[1].busy;
+assign rs_busy_st  = rs_table[2].busy;
+assign rs_busy_fp1 = rs_table[3].busy;
+assign rs_busy_fp2 = rs_table[4].busy;
 
 always_ff @(posedge clock){
-	if(reset)begin
-		int offset = 0;
-		for(int i = 0; i < 'NUM_FU_ALU;i++) begin
-			rs_table[i+offset].func_unit = ALU;
-		end
-		offset += 'NUM_FU_ALU;
-		for(int i = 0; i < 'NUM_FU_MULT;i++) begin
-			rs_table[i+offset].func_unit = MULT;
-		end
-		offset += 'NUM_FU_MULT;
-		for(int i = 0; i < 'NUM_FU_LOAD;i++) begin
-			rs_table[i+offset].func_unit = LD;
-		end
-		offset += 'NUM_FU_LOAD;
-		for(int i = 0; i < 'NUM_FU_STORE;i++) begin
-			rs_table[i+offset].func_unit = ST;
-		end
-	end else begin
-		if (op.wr_mem) begin
-			int offset = `NUM_FU_ALU + `NUM_FU_MULT + `NUM_FU_LOAD;
-			for(int i = offset;i < offset + `NUM_FU_STORE; i++) begin
-				if(!rs_table[i].busy) begin
-					rs_table[i].busy <= 1'b1;
-					rs_table[i].opcode <= op.inst[6:0];
-					break;
-				end
-			end
-		end else if (op.rd_mem) begin
-			int offset = `NUM_FU_ALU + `NUM_FU_MULT;
-			for(int i = offset;i < offset + `NUM_FU_LOAD; i++) begin
-				if(!rs_table[i].busy) begin
-					rs_table[i].busy <= 1'b1;
-					rs_table[i].opcode <= op.inst[6:0];
-					break;
-				end		
-			end
-		end else if (op.alu_func == ALU_MUL || op.alu_func == ALU_MULH || op.alu_func == ALU_MULHSU || op.alu_func == ALU_MULHU) begin
-			int offset = `NUM_FU_ALU;
-			for(int i = offset;i < offset + `NUM_FU_MULT; i++) begin
-				if(!rs_table[i].busy) begin
-					rs_table[i].busy <= 1'b1;
-					rs_table[i].opcode <= op.inst[6:0];
-					break;
-				end		
-			end
-		end else begin
-			int offset = 0;
-			for(int i = offset;i < offset + `NUM_FU_ALU; i++) begin
-				if(!rs_table[i].busy) begin
-					rs_table[i].busy <= 1'b1;
-					rs_table[i].opcode <= op.inst[6:0];
-					break;
-				end	
-			end
-		end
+	if (reset) begin 
+		rs_table[0].func_unit = ALU;
+		rs_table[1].func_unit = LD;
+		rs_table[2].func_unit = ST;
+		rs_table[3].func_unit = FP1;
+		rs_table[4].func_unit = FP2;
 	end
+	else begin 
+		if (op.wr_mem) begin // stf
+			if (!rs_table[2].busy) begin 
+				rs_table[2].busy <= 1'b1;
+				rs_table[2].opcode <= op.inst[6:0];
+				rs_table[2].T <= op.T;
+				rs_table[2].T1 <= op.T1;
+				rs_table[2].T2 <= op.T2;
+			end 
+		end
+		else if (op.rd_mem) begin // ldf
+			if (!rs_table[1].busy) begin 
+				rs_table[1].busy <= 1'b1;
+				rs_table[1].opcode <= op.inst[6:0];
+				rs_table[1].T <= op.T;
+				rs_table[1].T1 <= op.T1;
+				rs_table[1].T2 <= op.T2;
+			end
+		end
+		else if (op.alu_func == ALU_MUL || op.alu_func == ALU_MULH || op.alu_func == ALU_MULHSU || op.alu_func == ALU_MULHU) begin //mulf
+			if (!rs_table[3].busy) begin 
+				rs_table[3].busy <= 1'b1;
+				rs_table[3].opcode <= op.inst[6:0];
+				rs_table[3].T <= op.T;
+				rs_table[3].T1 <= op.T1;
+				rs_table[3].T2 <= op.T2;
+			end else if (rs_table[3].busy && !rs_table[4].busy) begin
+				rs_table[4].busy <= 1'b1;
+				rs_table[4].opcode <= op.inst[6:0];
+				rs_table[4].T <= op.T;
+				rs_table[4].T1 <= op.T1;
+				rs_table[4].T2 <= op.T2;
+			end
+		end 
+		else begin //addi
+			if (!rs_table[0].busy) begin 
+				rs_table[0].busy <= 1'b1;
+				rs_table[0].opcode <= op.inst[6:0];
+				rs_table[0].T <= op.T;
+				rs_table[0].T1 <= op.T1;
+				rs_table[0].T2 <= op.T2;
+			end
+		end
+		
+	end 
+		
+}
 
-	
-} 
 
 	
