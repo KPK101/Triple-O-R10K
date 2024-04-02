fea8283720ee7b8a9fb3c6a174d74e6f82e6d5f9
Rishit Thakkar <rht2122@columbia.edu>
2024-04-01 21:00:50 -0400

    CDB update, more additions required

--- verilog/cdb.sv
+++ verilog/cdb.sv
@@ -3,34 +3,17 @@
 
 
 module cdb(
-    input clock,
-    input reset,
-    input TAG Tin,
-    output TAG Tout,
-    output TAG Tout1,
-    output TAG Tout2
+    // input clock,
+    // input reset,
+    input en_rob,
+    input TAG rob_in,
+    output TAG rs_out
 );
 
-logic TAG T, T1;
-logic enable_cdb;
-
-rs rs_sta_1(
-    .clock(clock),
-    .reset(reset),
-    .T(Tout),
-    .T1(Tout1),
-    .T2(Tout2)
-)
 
 
-rob rob_1(
-    .clock(clock),
-    .reset(reset),
-)
-
-always_ff @(posedge clock) begin
-    if (reset) begin    
-        Tin <= 0;
+always_comb begin
+    if(en_rob)begin
+        rs_out = rob_in;
     end
-
 end
