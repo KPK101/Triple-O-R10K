a71c108cdc371b57955c283dbd50f452bab50bef
dawn-yoo <158089325+dawn-yoo@users.noreply.github.com>
2024-04-24 17:49:20 -0400

    Update sys_defs.svh
    changed order to sim

--- verilog/sys_defs.svh
+++ verilog/sys_defs.svh
@@ -24,7 +24,7 @@
 `define N 1
 
 // sizes
-`define ROB_SZ 0
+`define ROB_SZ 32
 `define RS_SZ 5
 `define PHYS_REG_SZ (32 + `ROB_SZ)
 
@@ -62,6 +62,7 @@
 // an instruction that does nothing instead of Zero which is really an ADDI x0, x0, 0
 `define NOP 32'h00000013
 
+
 //////////////////////////////////
 // ---- Memory Definitions ---- //
 //////////////////////////////////
@@ -69,7 +70,7 @@
 // Cache mode removes the byte-level interface from memory, so it always returns
 // a double word. The original processor won't work with this defined. Your new
 // processor will have to account for this effect on mem.
-// Notably, you can no longer write data without first reading.
+// Notably, you can no longer write data without first reading.ALU_OPA_SELECT opa_select,
 `define CACHE_MODE
 
 // you are not allowed to change this definition for your final processor
@@ -85,25 +86,6 @@
 
 `define MEM_SIZE_IN_BYTES (64*1024)
 `define MEM_64BIT_LINES   (`MEM_SIZE_IN_BYTES/8)
-
-////////////////////////////////////////////////////////////////////added by group members
- // definition of FU
-typedef enum logic [2:0] {
-    ALU,
-    LD,
-    ST,
-    FP1,
-    FP2
-} FU;
-
-typedef struct packed{
-        logic valid;
-	logic [$clog2(`PHYS_REG_SZ)-1:0] tag;
-	logic ready;
-} TAG;
-
-
-////////////////////////////////////////////////////////////////////
 
 typedef union packed {
     logic [7:0][7:0]  byte_level;
@@ -125,45 +107,11 @@ typedef enum logic [1:0] {
     BUS_STORE  = 2'h2
 } BUS_COMMAND;
 
-///////////////////////////////
-// ---- Exception Codes ---- //
-///////////////////////////////
-
-/**
- * Exception codes for when something goes wrong in the processor.
- * Note that we use HALTED_ON_WFI to signify the end of computation.
- * It's original meaning is to 'Wait For an Interrupt', but we generally
- * ignore interrupts in 470
- *
- * This mostly follows the RISC-V Privileged spec
- * except a few add-ons for our infrastructure
- * The majority of them won't be used, but it's good to know what they are
- */
-
-typedef enum logic [3:0] {
-    INST_ADDR_MISALIGN  = 4'h0,
-    INST_ACCESS_FAULT   = 4'h1,
-    ILLEGAL_INST        = 4'h2,
-    BREAKPOINT          = 4'h3,
-    LOAD_ADDR_MISALIGN  = 4'h4,
-    LOAD_ACCESS_FAULT   = 4'h5,
-    STORE_ADDR_MISALIGN = 4'h6,
-    STORE_ACCESS_FAULT  = 4'h7,
-    ECALL_U_MODE        = 4'h8,
-    ECALL_S_MODE        = 4'h9,
-    NO_ERROR            = 4'ha, // a reserved code that we use to signal no errors
-    ECALL_M_MODE        = 4'hb,
-    INST_PAGE_FAULT     = 4'hc,
-    LOAD_PAGE_FAULT     = 4'hd,
-    HALTED_ON_WFI       = 4'he, // 'Wait For Interrupt'.1 In 470, signifies the end of computation
-    STORE_PAGE_FAULT    = 4'hf
-} EXCEPTION_CODE;
-
 ///////////////////////////////////
 // ---- Instruction Typedef ---- //
 ///////////////////////////////////
 
-// from the RISC-V ISA spec
+// from the RISC-V ISA specALU_OPA_SELECT opa_select,
 typedef union packed {
     logic [31:0] inst;
     struct packed {
@@ -283,21 +231,174 @@ typedef enum logic [4:0] {
     ALU_REMU    = 5'h11  // unused
 } ALU_FUNC;
 
-////////////////////////////////
-// ---- Datapath Packets ---- //
-////////////////////////////////
 
-/**
- * Packets are used to move many variables between modules with
- * just one datatype, but can be cumbersome in some circumstances.
- *
- * Define new ones in project 4 at your own discretion
- */
+///////////////////////////////
+// ---- R10K Components ---- //
+///////////////////////////////
 
-/**
- * IF_ID Packet:1
- * Data exchanged from the IF to the ID stage
- */
+typedef struct packed{
+    logic valid;
+	logic [$clog2(`PHYS_REG_SZ)-1:0] phys_reg;
+	logic ready;
+} TAG;
+
+typedef struct packed{
+    INST              inst;
+    logic [`XLEN-1:0] PC;
+    logic [`XLEN-1:0] NPC; // PC + 4
+
+    ALU_OPA_SELECT opa_select; // ALU opa mux select (ALU_OPA_xxx *)
+    ALU_OPB_SELECT opb_select; // ALU opb mux select (ALU_OPB_xxx *)
+
+    logic [4:0] dest_reg_idx;  // destination (writeback) register index
+    ALU_FUNC    alu_func;      // ALU function select (ALU_xxx *)
+    logic       rd_mem;        // Does inst read memory?
+    logic       wr_mem;        // Does inst write memory?
+    logic       cond_branch;   // Is inst a conditional branch?
+    logic       uncond_branch; // Is inst an unconditional branch?
+    logic       halt;          // Is this a halt?OPB_IS_I_IMM
+    logic       illegal;       // Is this instruction illegal?
+    logic       csr_op;        // Is this a CSR operation? (we use this to get return code)
+    TAG			t;
+    TAG			t1;
+    TAG			t2;
+    logic [$clog2(`RS_SZ)-1:0] rs_idx;
+    logic [$clog2(`ROB_SZ)-1:0] rob_idx;
+    
+    logic       valid;
+} DECODER_PACKET;
+
+/////////////////////////////////
+// ---- Map Table Packets ---- //
+/////////////////////////////////
+
+//ID -> MT Connections
+typedef struct packed{
+	//Query param
+	logic [4:0] read_idx_1;
+	logic [4:0] read_idx_2;
+	
+	//Write param
+	logic [4:0] write_idx;
+	TAG write_tag;
+	logic write_en;
+}ID_MT_PACKET;
+
+
+
+//MT -> ID Connections
+typedef struct packed{
+	//Query results
+	TAG read_out_1;
+	TAG read_out_2;
+	TAG write_out;
+}MT_ID_PACKET;
+
+///////////////////////////////////////////
+// ---- Reservation Station Packets ---- //
+///////////////////////////////////////////
+
+
+//ID -> RS
+typedef struct packed{
+	DECODER_PACKET decoder_packet;
+	logic write_en;
+}ID_RS_PACKET;
+
+//EX -> RS
+typedef struct packed{
+	logic [$clog2(`RS_SZ)-1:0] remove_idx;
+	logic remove_en;
+}EX_RS_PACKET;
+
+//RS -> ID
+typedef struct packed{
+	logic [$clog2(`RS_SZ)-1:0] free_idx;
+	logic free;
+}RS_ID_PACKET;
+
+//RS -> IS
+typedef struct packed{
+	DECODER_PACKET decoder_packet;
+	logic issue_en;
+}RS_IS_PACKET;
+
+
+//////////////////////////////////////
+// ---- Reorder Buffer Packets ---- //
+//////////////////////////////////////
+
+typedef struct packed{
+    TAG t_in;
+    TAG t_old_in;
+    logic write_en;
+}ID_ROB_PACKET;
+
+typedef struct packed{
+    logic [$clog2(`ROB_SZ)-1:0] complete_idx;
+    logic complete_en;
+}IC_ROB_PACKET;
+
+typedef struct packed{
+    logic [$clog2(`ROB_SZ)-1:0] free_idx;
+    logic free;
+}ROB_ID_PACKET;
+
+typedef struct packed{
+    TAG retire_t;
+    TAG retire_t_old;
+    logic retire_en;
+}ROB_IR_PACKET;
+
+/////////////////////////////////
+// ---- Free List Packets ---- //
+/////////////////////////////////
+typedef struct packed{
+    logic pop_en;
+}ID_FL_PACKET;
+
+typedef struct packed{
+    TAG retire_t_old;
+    logic retire_en;
+}IR_FL_PACKET;
+
+typedef struct packed{
+    TAG retire_t;
+    TAG retire_t_old;
+    logic retire_en;
+}IR_AM_PACKET;
+
+typedef struct packed{
+    TAG free_tag;
+    logic free;
+}FL_ID_PACKET;
+
+//////////////////////////////////////////////
+// ---- Physical Register File Packets ---- //
+//////////////////////////////////////////////
+
+//TODO: THIS IC_PRF IS TEMPORARY (WHAT IF YOU NEED TO COMPLETE MORE THAN ONE??)
+typedef struct packed{
+    TAG write_tag;
+    logic [`XLEN-1:0] write_data;
+    logic write_en;
+}IC_PRF_PACKET;
+
+typedef struct packed{
+    TAG read_tag_1;
+    TAG read_tag_2;
+}IS_PRF_PACKET;
+
+typedef struct packed{
+    logic [`XLEN-1:0] read_out_1;
+    logic [`XLEN-1:0] read_out_2;
+}PRF_IS_PACKET;
+
+
+/////////////////////////////
+// ---- Stage Packets ---- //
+/////////////////////////////
+
 typedef struct packed {
     INST              inst;
     logic [`XLEN-1:0] PC;
@@ -305,10 +406,7 @@ typedef struct packed {
     logic             valid;
 } IF_ID_PACKET;
 
-/**
- * ID_EX Packet:
- * Data exchanged from the ID to the EX stage
- */
+//TODO: THIS CODE IS A PLACEHOLDER.
 typedef struct packed {
     INST              inst;
     logic [`XLEN-1:0] PC;
@@ -320,7 +418,7 @@ typedef struct packed {
     ALU_OPA_SELECT opa_select; // ALU opa mux select (ALU_OPA_xxx *)
     ALU_OPB_SELECT opb_select; // ALU opb mux select (ALU_OPB_xxx *)
 
-    logic [4:0] dest_reg_idx;  // destination (writeback) register index
+    TAG dest_tag;  // destination tag
     ALU_FUNC    alu_func;      // ALU function select (ALU_xxx *)
     logic       rd_mem;        // Does inst read memory?
     logic       wr_mem;        // Does inst write memory?
@@ -328,17 +426,14 @@ typedef struct packed {
     logic       uncond_branch; // Is inst an unconditional branch?
     logic       halt;          // Is this a halt?
     logic       illegal;       // Is this instruction illegal?
-    logic       csr_op;        // Is this a CSR operation? (we use this to get return code)
-    logic       valid;
-    TAG		T;
-    TAG		T1;
-    TAG		T2;
-} ID_EX_PACKET;
+    logic       csr_op;        // Is this a CSR operation? (we only used this as a cheap way to get return code)
+    logic [$clog2(`RS_SZ)-1:0] rs_idx;
+    logic [$clog2(`ROB_SZ)-1:0] rob_idx;
 
-/**
- * EX_MEM Packet:
- * Data exchanged from the EX to the MEM stage
- */
+    logic       valid;
+} IS_EX_PACKET;
+
+
 typedef struct packed {
     logic [`XLEN-1:0] alu_result;
     logic [`XLEN-1:0] NPC;
@@ -354,27 +449,50 @@ typedef struct packed {
     logic             csr_op;
     logic             rd_unsigned; // Whether proc2Dmem_data is signed or unsigned
     MEM_SIZE          mem_size;
+    logic [$clog2(`RS_SZ)-1:0] rs_idx;
+    logic [$clog2(`ROB_SZ)-1:0] rob_idx;
+    
     logic             valid;
-} EX_MEM_PACKET;
+} EX_IC_PACKET;
+
+
+
+///////////////////////////////
+// ---- Exception Codes ---- //
+///////////////////////////////
 
 /**
- * MEM_WB Packet:
- * Data exchanged from the MEM to the WB stage
+ * Exception codes for when something goes wrong in the processor.
+ * Note that we use HALTED_ON_WFI to signify the end of computation.
+ * It's original meaning is to 'Wait For an Interrupt', but we generally
+ * ignore interrupts in 470
  *
- * Does not include data sent from the MEM stage to memory
+ * This mostly follows the RISC-V Privileged spec
+ * except a few add-ons for our infrastructure
+ * The majority of them won't be used, but it's good to know what they are
  */
-typedef struct packed {
-    logic [`XLEN-1:0] result;
-    logic [`XLEN-1:0] NPC;
-    logic [4:0]       dest_reg_idx; // writeback destination (ZERO_REG if no writeback)
-    logic             take_branch;
-    logic             halt;    // not used by wb stage
-    logic             illegal; // not used by wb stage
-    logic             valid;
-} MEM_WB_PACKET;
 
-/**
- * No WB output packet as it would be more cumbersome than useful
- */
+typedef enum logic [3:0] {
+    INST_ADDR_MISALIGN  = 4'h0,
+    INST_ACCESS_FAULT   = 4'h1,
+    ILLEGAL_INST        = 4'h2,
+    BREAKPOINT          = 4'h3,
+    LOAD_ADDR_MISALIGN  = 4'h4,
+    LOAD_ACCESS_FAULT   = 4'h5,
+    STORE_ADDR_MISALIGN = 4'h6,
+    STORE_ACCESS_FAULT  = 4'h7,
+    ECALL_U_MODE        = 4'h8,
+    ECALL_S_MODE        = 4'h9,
+    NO_ERROR            = 4'ha, // a reserved code that we use to signal no errors
+    ECALL_M_MODE        = 4'hb,
+    INST_PAGE_FAULT     = 4'hc,
+    LOAD_PAGE_FAULT     = 4'hd,
+    HALTED_ON_WFI       = 4'he, // 'Wait For Interrupt'.1 In 470, signifies the end of computation
+    STORE_PAGE_FAULT    = 4'hf
+} EXCEPTION_CODE;
+
+
+
+
  
 `endif // __SYS_DEFS_SVH__
