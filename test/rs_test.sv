f80a4c975ecbc2b46188eaaf19c1c50fbeb913ac
dawn-yoo <dy2486@columbia.edu>
2024-04-28 18:51:14 -0400

    rs free idx handling checked

--- test/rs_test.sv
+++ test/rs_test.sv
@@ -9,7 +9,10 @@ module rs_test;
 	ID_RS_PACKET id_rs; // decoder_packet, write_en
 	EX_RS_PACKET ex_rs; // remove_idx, remove_en
 	logic cdb_en; 
-	TAG cdb; // valid, phys_reg, ready
+	logic interrupt;
+	logic [4:0] rs_busy;
+	logic is_mult;
+	//TAG cdb; // valid, phys_reg, ready
 
 	RS_ID_PACKET rs_id; // free_idx, free
 	RS_IS_PACKET rs_is; // decoder_packet, issue_en
@@ -17,14 +20,16 @@ module rs_test;
 	rs rs_dut (
 		.clock(clock), 
 		.reset(reset),
-		.cdb(cdb),
+		//.cdb(cdb),
+		.interrupt(interrupt),
 		.cdb_en(cdb_en),
+		.rs_busy_status(rs_busy),
+		.is_mult(is_mult),
 		.id_rs_packet(id_rs), 
 		.ex_rs_packet(ex_rs), 
 		.rs_id_packet(rs_id), 
 		.rs_is_packet(rs_is)
 	);
-
 
 
     // CLOCK_PERIOD is defined on the commandline by the makefile
@@ -35,44 +40,103 @@ module rs_test;
 
    initial begin
         // setup monitor and reset signals
-        $monitor("time: %3.0d reset : %b id_write_en: %b id_decoder.rd_mem: %b ex.remove_idx: %b ex.remove_en: %b rs_id.free_idx: %b rs_id.free: %b rs_is.decoder_packet %d rs_is.issue_en: %b \n",
-                 $time, reset, id_rs.write_en, id_rs.decoder_packet.rd_mem, ex_rs.remove_idx, ex_rs.remove_en, rs_id.free_idx, rs_id.free, rs_is.decoder_packet, rs_is.issue_en);
+        $monitor("time: %3.0d reset : %b id_write_en: %b rd_mem: %b wr_mem: %b ex.remove_idx: %d ex.remove_en: %b rs_id.free_idx: %d rs_id.free: %b rs_is.issue_en: %b rs_busy: %5.0b mult?: %b \n",
+                 $time, reset, id_rs.write_en, id_rs.decoder_packet.rd_mem, id_rs.decoder_packet.wr_mem, ex_rs.remove_idx, ex_rs.remove_en, rs_id.free_idx, rs_id.free, rs_is.issue_en, rs_busy, is_mult);
 
         clock = 1'b0;
+	@(posedge clock)
         reset = 1'b1;
-	@(negedge clock)
+	@(posedge clock)
 	reset = 1'b0;
+	@(posedge clock)
 
-	/*first index*/
+	// first 105 - read
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
 	id_rs.write_en = 1'b1;
-	id_rs.decoder_packet.rd_mem = 1; // write decoder packet 
+	id_rs.decoder_packet.wr_mem = 0;  
+	id_rs.decoder_packet.rd_mem = 1; // free_idx 1
 	ex_rs.remove_idx = 0;
 	ex_rs.remove_en =0;
 	
-	//second index
-	#10
+	// second 135 - write 
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
+	id_rs.write_en = 1'b1;
+	id_rs.decoder_packet.wr_mem = 1; // free_idx 2 
+	//id_rs.decoder_packet.alu_func = ALU_MUL; // index 4
 	ex_rs.remove_idx = 0;
+	ex_rs.remove_en =0;
+	
+	// third 165 - read (should not be free)
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
+	id_rs.write_en = 1'b1;
+	id_rs.decoder_packet.wr_mem = 0; // free_idx 2 
+	id_rs.decoder_packet.rd_mem = 1; // free_idx 1
+	ex_rs.remove_idx = 0;
+	ex_rs.remove_en =0;
 
-	/*//third index
-	#10
+	// fourth 195 - alu mult 
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
+	id_rs.write_en = 1'b1;
+	id_rs.decoder_packet.wr_mem = 0; // free_idx 2 
+	id_rs.decoder_packet.rd_mem = 0; // free_idx 1
+	id_rs.decoder_packet.alu_func=ALU_MUL; // index 3 
+	ex_rs.remove_idx = 0;
+	ex_rs.remove_en =0;
 
-	//fourth index
-	#10
+	// fifth 225 - read remove
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
+	id_rs.write_en = 1'b0;
+	id_rs.decoder_packet.wr_mem = 0; // free_idx 2 
+	id_rs.decoder_packet.rd_mem = 0; // free_idx 1
+	ex_rs.remove_idx = 1;
+	ex_rs.remove_en =1;
+	
+	// sixth 255 - non mult alu 
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
+	id_rs.write_en = 1'b1;
+	id_rs.decoder_packet.wr_mem = 0; // free_idx 2 
+	id_rs.decoder_packet.rd_mem = 0; // free_idx 1
+	id_rs.decoder_packet.alu_func=ALU_ADD; // index 0 
+	ex_rs.remove_idx = 0;
+	ex_rs.remove_en =0;
 
-	//fifth index
-	#10
+	// seventh 285 - alu mult 
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
+	id_rs.write_en = 1'b1;
+	id_rs.decoder_packet.wr_mem = 0; // free_idx 2 
+	id_rs.decoder_packet.rd_mem = 0; // free_idx 1
+	id_rs.decoder_packet.alu_func=ALU_MUL; // index 4 
+	ex_rs.remove_idx = 0;
+	ex_rs.remove_en =0;
 
+	// eight 315 - cdb 
+	@(posedge clock)
+	interrupt = 1'b0;
+	cdb_en = 1'b0;
+	id_rs.write_en = 1'b1;
+	id_rs.decoder_packet.wr_mem = 0; // free_idx 2 
+	id_rs.decoder_packet.rd_mem = 0; // free_idx 1
+	id_rs.decoder_packet.alu_func=ALU_MUL; // index 4 
+	ex_rs.remove_idx = 0;
+	ex_rs.remove_en =0;
 
-	//sixth index
-	#10
+	@(posedge clock)
 
-
-	#10 
-
-	#10*/
-
-
-	    #20;
+	#60	    
     $display("\nENDING TESTBENCH: SUCCESS!");
     $display("@@@ Passed\n");
     $finish; // End simulation
--- verilog/rs.sv
+++ verilog/rs.sv
@@ -14,24 +14,41 @@ module rs(
 	input interrupt,
 	input TAG cdb,
 	input cdb_en,
-	input ID_RS_PACKET id_rs_packet,
-	input EX_RS_PACKET ex_rs_packet,
+	input ID_RS_PACKET id_rs_packet, // decoder_packet + write_en 
+	input EX_RS_PACKET ex_rs_packet, // remove_idx + remove_en
 	
-	output RS_ID_PACKET rs_id_packet,
-	output RS_IS_PACKET rs_is_packet
+	output [4:0] rs_busy_status, // 5 bit 0 -> 4 for debug purpose
+	output logic is_mult,
+	output RS_ID_PACKET rs_id_packet, 
+	output RS_IS_PACKET rs_is_packet 
 );
 	
 	RS_ENTRY [`RS_SZ - 1:0] rs_table;
 
 	//Handle rs -> id logic
-	logic is_mult;
+	//logic is_mult;
 	assign is_mult = id_rs_packet.decoder_packet.alu_func == ALU_MUL || id_rs_packet.decoder_packet.alu_func == ALU_MULH ||
 			 id_rs_packet.decoder_packet.alu_func == ALU_MULHSU || id_rs_packet.decoder_packet.alu_func == ALU_MULHU;
-	
+
 	assign rs_id_packet.free_idx   = id_rs_packet.decoder_packet.wr_mem ? 2 :
 					 id_rs_packet.decoder_packet.rd_mem ? 1 :
 					 !is_mult			    ? 0 :
-					 rs_table[3].busy	            ? 4 : 3; // switched 3 and 4
+					 rs_table[3].busy	            ? 4 : 3; 
+	
+	// for debugging only 
+	assign rs_busy_status[0] = rs_table[0].busy ; 
+	assign rs_busy_status[1] = rs_table[1].busy ;
+	assign rs_busy_status[2] = rs_table[2].busy ;
+	assign rs_busy_status[3] = rs_table[3].busy ;
+	assign rs_busy_status[4] = rs_table[4].busy ;
+	
+	/*
+		0: alu (not mult)
+		1: load (read_mem) 
+		2: store (write_mem) 
+		3: fp1 
+		4: fp2  	
+	*/
 
 	assign rs_id_packet.free = !rs_table[rs_id_packet.free_idx].busy; 
 
@@ -43,7 +60,7 @@ module rs(
 			   (!rs_table[i].decoder_packet.t1.valid || rs_table[i].decoder_packet.t1.ready) && 
 			   (!rs_table[i].decoder_packet.t2.valid || rs_table[i].decoder_packet.t2.ready)) begin
 				rs_is_packet.decoder_packet = rs_table[i].decoder_packet;
-				rs_is_packet.issue_en = 1;
+				rs_is_packet.issue_en = 1;  
 			end
 		end
 	end
